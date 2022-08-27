8744ae7346829a9f27c32cc2f59e04537134e20e
Benjamin <benseed.bs@gmail.com>
2022-08-27 12:51:52 +0100

    Created itineraries that have ports and refactored test suits

--- src/Ship.js
+++ src/Ship.js
@@ -1,18 +1,31 @@
+const Itinerary = require("./Itinerary");
 
-class Ship {
-    constructor(currentPort) {
-        this.currentPort = currentPort;
-    };
+    class Ship {
+    constructor(itinerary) {
+        this.itinerary = itinerary;
+        this.currentPort = itinerary.ports[0];
+        this.previousPort = null;
+    }
 
     setSail() {
+        const itinerary = this.itinerary;
+        const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
+
+        if (currentPortIndex === (itinerary.ports.length - 1)) {
+            throw new Error('End of itinerary reached');
+        }
+
+        this.previousPort = this.currentPort;
         this.currentPort = null;
     }
     // see dependency inversion
     dock(port) {
-        this.currentPort = port;
-    }
-};
-// a falsy value is false, 0, null, NaN etc
+        const itinerary = this.itinerary;
+        const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+        this.currentPort = itinerary.ports[previousPortIndex + 1];
+    };
+    };
+    // a falsy value is false, 0, null, NaN etc
 
 
 module.exports = Ship;
