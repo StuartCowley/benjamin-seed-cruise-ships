b85d18e2a8fe2e31794561326a9be564bf3cb32d
Benjamin <benseed.bs@gmail.com>
2022-08-31 20:09:11 +0100

    DRYing ship.test and port.test suites

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -1,29 +1,34 @@
 const Port = require('../src/Port.js')
 
 describe('Port', () => {
+    describe('with ports', () => {
+        let port;
+        let ship;
+        let titanic;
+        let queenMary;
+    })
+
+    beforeEach(() => {
+         port = new Port('Dover');
+         ship = {};
+         titanic = {};
+         queenMary = {};
+    })
+
     it('can be instantiated', () => {
         expect(new Port()).toBeInstanceOf(Object);
     });
 
     it('port object has a name', () => {
-        const port = new Port('Dover');
         expect(port.name).toEqual('Dover');
     });
 
     it('can add a ship', () => {
-        const port = new Port('Dover');
-        const ship = {};
-
         port.addShip(ship);
-
         expect(port.ships).toContain(ship);
     })
 
     it('can remove a ship', () => {
-        const port = new Port('Dover');
-        const titanic = {};
-        const queenMary = {};
-
         port.addShip(titanic);
         port.addShip(queenMary);
         port.removeShip(queenMary);
